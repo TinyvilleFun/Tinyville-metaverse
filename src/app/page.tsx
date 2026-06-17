2d786818d135bab75f6a8ce4f7cbdf6732aba643
TinyvilleFun <cingur251@gmail.com>
2026-06-17 15:16:42 +0700

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex flex-col justify-center items-center">
+      <h1 className="text-7xl font-bold">
+        🏡 Tinyville
+      </h1>
+
+      <p className="text-xl mt-4">
+        Tiny Town. Big Adventures.
+      </p>
+    </main>
+  );
+}
